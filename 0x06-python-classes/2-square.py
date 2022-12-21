e151e07fbc730b6e0d6c036e81f224fedd457464
MilkisaS <MilkisaS>
2022-12-21 06:40:25 -0800

    This i task number 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+# 2-sqaure.py
+
+class Square:
+
+    def __init__(self, size=0):
+
+        self.__size = size
+
+        try:
+            if type is not integer
+
+        except TypeError:
+            print('size must be an integer')
+        else if size is less than 0
+        except ValueError:
+            print('size must be >= 0')
+
+def main:
+
+main()
+
