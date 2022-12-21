28342523a3608445cd512f992082789d9a464e60
MilkisaS <MilkisaS>
2022-12-21 10:58:20 -0800

    Task number 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 
 # 1-square.py
+    """Define a class Square"""
 
 class Square:
-
+"""Represent Square """
     def __init__(self, size):
+""" size is going to be declared
+
+
+Args:
+    size (int): This is the type of size
+    """
 
         self.__size = size
-
-def main:
-
-main()
