35bfc688800d8560055f5eb08d04ad472398e8a8
MilkisaS <MilkisaS>
2022-12-25 01:26:55 -0800

    Task 2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 2-replace_in_list.py
+
+"""This is the prototype"""
+
+def replace_in_list(my_list, idx, element):
+    """Is used to replace an element at specific place"""
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx] = element
