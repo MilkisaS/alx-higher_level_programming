a96e03318e982c45e6c73eecd86efb4130911052
MilkisaS <MilkisaS>
2022-12-25 00:58:17 -0800

    Task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,15 +4,17 @@
 """ Description of the prototype"""
 
 def element_at(my_list, idx):
-	"""Definition of args and value"""
+    """Definition of args and value"""
     if idx < 0:
         return None
     else if idx > len(my_list):
         return None
     else:
-	return my_list[idx]
-	""" 	This is going to return the element of index	
-	Args:
-	my_list: is the list name
-	idx: is the index number
-	"""
+	return my_list[idx]	
+    
+    """This is going to return the element of index	
+    
+    Args:
+    my_list: is the list name
+    idx: is the index number
+    """
