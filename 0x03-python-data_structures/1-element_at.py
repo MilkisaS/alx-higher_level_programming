fea2e693a32127edeef58ffecc13344af473a9c6
MilkisaS <MilkisaS>
2022-12-25 00:50:33 -0800

    Task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# 1-element_at.py
+
+""" Description of the prototype"""
+
+def element_at(my_list, idx):
+	"""Definition of args and value"""
+
+	return (my_list[idx])
+	""" 	This is going to return the element of index	
+	Args:
+	my_list: is the list name
+	idx: is the index number
+	"""
+def main():
+
+main()
