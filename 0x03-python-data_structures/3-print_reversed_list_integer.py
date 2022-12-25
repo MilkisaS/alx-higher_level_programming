5483dd3f2da693bb7ea48dfedbb44f17da07d190
MilkisaS <MilkisaS>
2022-12-25 05:33:38 -0800

    Task 3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 3-print_reversed_list_integer.py
+
+def print_reversed_list_integer(my_list=[]):
+    """This a code used to reverse an element of a list"""
+
+    my_list.reverse()
+    return (my_list)
