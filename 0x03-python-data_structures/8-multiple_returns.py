a5df4a9c08451f839f3f7c059a1a114f9a31ba6a
MilkisaS <MilkisaS>
2022-12-25 09:27:31 -0800

    Task number 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 8-multiple_returns.py
+
+def multiple_returns(sentence):
+	count = len(sentence)
+        if count == 0:
+            first = None
+            return count, first
+        else:
+            first == sentence[0]
+            return count, first
